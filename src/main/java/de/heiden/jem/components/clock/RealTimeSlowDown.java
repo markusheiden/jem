ab0d0de51d8ab5fd1cdecd37013dc904d611e4ff
Markus Heiden <markus@markusheiden.de>
2010-10-08 00:23:18 +0000

    mh: reworked real time slow down

--- src/main/java/de/heiden/jem/components/clock/RealTimeSlowDown.java
+++ src/main/java/de/heiden/jem/components/clock/RealTimeSlowDown.java
@@ -16,13 +16,26 @@ public class RealTimeSlowDown implements ClockEvent
   private final long _freq;
   private final int _div;
   private final long _incrementFreq;
+  private final long _incrementFreq0;
   private final long _incrementTime;
-  private final long _incrementFreq1;
+  private final long _incrementTime0;
 
+  /**
+   * Timestamp (ns) after last slow down.
+   */
   private long _last;
-  private long _elapsed;
-  private int _counter;
+  /**
+   * Timestamp (ns) for next slow down.
+   */
   private long _next;
+  /**
+   * Time (ns) elapsed while processing.
+   */
+  private long _elapsed;
+  /**
+   * Counter for divs.
+   */
+  private int _counter;
 
   /**
    * Constructor.
@@ -42,49 +55,57 @@ public class RealTimeSlowDown implements ClockEvent
     _freq = freq;
     _div = div;
     _incrementFreq = freq / div;
-    _incrementFreq1 = freq - (div - 1) * _incrementFreq;
+    _incrementFreq0 = freq - (div - 1) * _incrementFreq;
     _incrementTime = 1000000000 / div;
+    _incrementTime0 = 1000000000 - (div - 1) * _incrementTime;
 
     long now = System.nanoTime();
     _last = now;
     _elapsed = 0;
-    _counter = div;
-    _next = now + _incrementTime;
+    _counter = 0;
+    _next = now;
 
-    _clock.addClockEvent(0 + _incrementFreq1, this);
+    _clock.addClockEvent(0, this);
   }
 
   @Override
   public void execute(long tick)
   {
+    // Save the current timestamp as the end of the last processing cycle
     long now = System.nanoTime();
-    _elapsed += (now - _last) / 1000000;
+    // Compute duration of the last processing cycle and
+    // add it to the duration of processing 1 second
+    _elapsed += now - _last;
 
-    long incrementTick;
-    if (_counter == 1)
+    long nextTick;
+    if (_counter == 0)
     {
-      _logger.info("1 simulated second took " + _elapsed + " ms");
+      _logger.info("1 simulated second took " + (_elapsed / 1000000) + " ms");
       _elapsed = 0;
-      _counter = _div;
-      incrementTick = _incrementFreq1;
+
+      _next += _incrementTime0;
+      nextTick = tick + _incrementFreq0;
+      _counter = _div - 1;
     }
     else
     {
+      _next += _incrementTime;
+      nextTick = tick + _incrementFreq;
       _counter--;
-      incrementTick = _incrementFreq;
     }
-    _clock.addClockEvent(tick + incrementTick, this);
+    _clock.addClockEvent(nextTick, this);
 
     if (_logger.isDebugEnabled())
     {
       _logger.debug("tick     : " + tick);
-      _logger.debug("next tick: " + (tick + incrementTick));
-      _logger.error("elapsed  : " + ((now - _last) / 1000000) + " ms");
-      _logger.debug("remainder: " + ((_next - now) / 1000000) + " ms");
+      _logger.debug("next tick: " + nextTick);
+      _logger.error("elapsed  : " + (now - _last) + " ns");
+      _logger.debug("remainder: " + (_next - now) + " ns");
     }
 
+    // Wait until _next
     long remainder;
-    while ((remainder = (_next - now) / 1000000) > 1) // 1 milli second precision
+    while ((remainder = (_next - now) / 1000000) > 0) // 1 milli second precision
     {
       try
       {
@@ -97,7 +118,7 @@ public class RealTimeSlowDown implements ClockEvent
       }
     }
 
+    // Save the current timestamp as the start of next the processing cycle
     _last = now;
-    _next += _incrementTime;
   }
 }
