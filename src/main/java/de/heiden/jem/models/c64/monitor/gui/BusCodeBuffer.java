ff17116a4a74fb95f31035c86574f129cc545e23
Markus Heiden <markus@markusheiden.de>
2010-10-10 09:40:24 +0000

    mh: extracted ICodeBuffer to allow different implementations

---
+++ src/main/java/de/heiden/jem/models/c64/monitor/gui/BusCodeBuffer.java
@@ -0,0 +1,32 @@
+package de.heiden.jem.models.c64.monitor.gui;
+
+import de.heiden.c64dt.assembler.AbstractCodeBuffer;
+import de.heiden.jem.components.bus.BusDevice;
+
+/**
+ * Code buffer based on a bus.
+ */
+public class BusCodeBuffer extends AbstractCodeBuffer
+{
+  private final BusDevice bus;
+
+  /**
+   * Constructor.
+   *
+   * @param position start address of the code disassemble
+   * @param bus bus
+   */
+  public BusCodeBuffer(int position, BusDevice bus)
+  {
+    super(0x0000, 0x10000);
+
+    this.position = position;
+    this.bus = bus;
+  }
+
+  @Override
+  public int readByte()
+  {
+    return bus.read(position++);
+  }
+}
