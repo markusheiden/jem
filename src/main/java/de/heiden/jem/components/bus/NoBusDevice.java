f2cfb9e438d1a2092d8028ef6c7e4808d1994591
Markus Heiden <markus@markusheiden.de>
2010-10-11 22:43:48 +0000

    mh: optimized

--- src/main/java/de/heiden/jem/components/bus/NoBusDevice.java
+++ src/main/java/de/heiden/jem/components/bus/NoBusDevice.java
@@ -3,16 +3,16 @@ package de.heiden.jem.components.bus;
 /**
  * Bus device for an "open" bus, when no bus device has been selected.
  */
-public class NoBusDevice implements BusDevice
+public final class NoBusDevice implements BusDevice
 {
   @Override
-  public int read(int address)
+  public final int read(int address)
   {
     return 0xFF;
   }
 
   @Override
-  public void write(int value, int address)
+  public final void write(int value, int address)
   {
     // ignored
   }
