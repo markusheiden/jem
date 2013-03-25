9441d8f59d33701f8103013c9095674d38703e09
Markus Heiden <markus@markusheiden.de>
2013-03-26 00:21:52 +0100

    Reworked output capturing

--- src/main/java/de/heiden/jem/models/c64/components/patch/SystemOut.java
+++ src/main/java/de/heiden/jem/models/c64/components/patch/SystemOut.java
@@ -1,5 +1,6 @@
 package de.heiden.jem.models.c64.components.patch;
 
+import de.heiden.c64dt.charset.C64Charset;
 import de.heiden.jem.components.bus.BusDevice;
 import de.heiden.jem.models.c64.components.cpu.CPU6510State;
 import de.heiden.jem.models.c64.components.cpu.Patch;
@@ -9,6 +10,11 @@ import de.heiden.jem.models.c64.components.cpu.Patch;
  */
 public class SystemOut extends Patch {
   /**
+   * Charset.
+   */
+  private C64Charset charset = C64Charset.LOWER;
+
+  /**
    * Constructor.
    */
   public SystemOut() {
@@ -17,7 +23,28 @@ public class SystemOut extends Patch {
 
   @Override
   protected int execute(CPU6510State state, BusDevice bus) {
-    System.out.print(Character.valueOf((char) state.A));
+//    if ("q".equals(charset.toString((byte) state.A))) {
+//      System.out.println(">" + Integer.toHexString(state.A) + "<");
+//    }
+    String result;
+    switch (state.A) {
+      case 0x0a:
+        result = "\r";
+        break;
+      case 0x0d:
+        result = "\n";
+        break;
+      case 0x91:
+        result = "";
+        break; // cursor up -> ignore
+      case 0x93:
+        result = "";
+        break; // clr/home -> ignore
+      default:
+        result = charset.toString((byte) state.A);
+    }
+
+    System.out.print(result);
     state.C = false; // success
     return replaced;
   }
