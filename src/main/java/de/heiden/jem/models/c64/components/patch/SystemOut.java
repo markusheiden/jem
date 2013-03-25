112ccf98e0999efa261f92cc0979112e7ac0f946
Markus Heiden <markus@markusheiden.de>
2013-03-26 00:27:41 +0100

    Reworked output capturing

--- src/main/java/de/heiden/jem/models/c64/components/patch/SystemOut.java
+++ src/main/java/de/heiden/jem/models/c64/components/patch/SystemOut.java
@@ -5,6 +5,10 @@ import de.heiden.jem.components.bus.BusDevice;
 import de.heiden.jem.models.c64.components.cpu.CPU6510State;
 import de.heiden.jem.models.c64.components.cpu.Patch;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
 /**
  * Replaces standard C64 "System.out" at $E716 (part of $FFD2)
  */
@@ -15,10 +19,26 @@ public class SystemOut extends Patch {
   private C64Charset charset = C64Charset.LOWER;
 
   /**
+   * Writer to write output to.
+   */
+  private final Writer writer;
+
+  /**
    * Constructor.
    */
   public SystemOut() {
+    this(new PrintWriter(System.out));
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param writer Writer to write output to
+   */
+  public SystemOut(Writer writer) {
     super(0xE716);
+
+    this.writer = writer;
   }
 
   @Override
@@ -44,7 +64,13 @@ public class SystemOut extends Patch {
         result = charset.toString((byte) state.A);
     }
 
-    System.out.print(result);
+    try {
+      writer.write(result);
+      writer.flush();
+    } catch (IOException e) {
+      logger.error("Failed to write to console", e);
+    }
+
     state.C = false; // success
     return replaced;
   }
