684fcd767044e6703b4f6ed7e3b294cc10b91a0f
Markus Heiden <markus@markusheiden.de>
2013-03-25 21:08:30 +0100

    Switched to logback

---
+++ src/test/java/de/heiden/jem/models/c64/TestC64.java
@@ -0,0 +1,71 @@
+package de.heiden.jem.models.c64;
+
+import de.heiden.jem.components.clock.Clock;
+import de.heiden.jem.components.clock.serialthreads.SerialClock;
+import de.heiden.jem.models.c64.components.CIA6526;
+import de.heiden.jem.models.c64.components.cpu.C64Bus;
+import de.heiden.jem.models.c64.components.cpu.CPU6510;
+import de.heiden.jem.models.c64.components.keyboard.Keyboard;
+import de.heiden.jem.models.c64.components.memory.ColorRAM;
+import de.heiden.jem.models.c64.components.memory.RAM;
+import de.heiden.jem.models.c64.components.memory.ROM;
+import de.heiden.jem.models.c64.components.vic.VIC6569PAL;
+import de.heiden.jem.models.c64.components.vic.VICBus;
+import de.heiden.jem.models.c64.util.ROMLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Modified C64 for better testability.
+ */
+public class TestC64 {
+  /**
+   * Logger.
+   */
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  private final Clock _clock;
+
+  private C64Bus _cpuBus;
+  private final CPU6510 _cpu;
+
+  private Keyboard _keyboard;
+  private final VIC6569PAL _vic;
+
+  /**
+   * Constructor.
+   */
+  public TestC64() throws Exception {
+    _clock = new SerialClock();
+
+    RAM _ram = new RAM(0x10000);
+    ColorRAM colorRam = new ColorRAM(0x400);
+    ROM basic = ROMLoader.basic(ROMLoader.DEFAULT_BASIC);
+    ROM kernel = ROMLoader.kernel(ROMLoader.DEFAULT_KERNEL);
+    ROM charset = ROMLoader.character(ROMLoader.DEFAULT_CHARACTER);
+
+    // Patch file read routine to be executed in java
+    kernel.patch(0x100, 0xF4A5);
+
+    CIA6526 cia1 = new CIA6526(_clock);
+    CIA6526 cia2 = new CIA6526(_clock);
+
+    VICBus vicBus = new VICBus(cia2.portA(), _ram, charset);
+    _vic = new VIC6569PAL(_clock, vicBus, colorRam);
+
+    _keyboard = new Keyboard(cia1.portA(), cia1.portB());
+
+    _cpu = new CPU6510(_clock);
+    _cpuBus = new C64Bus(_cpu.getPort(), _ram, basic, _vic, colorRam, cia1, cia2, charset, kernel);
+    _cpu.connect(_cpuBus);
+    _cpu.getIRQ().connect(cia1.getIRQ());
+    _cpu.getIRQ().connect(_vic.getIRQ());
+    _cpu.getNMI().connect(cia2.getIRQ());
+    _cpu.getNMI().connect(_keyboard.getNMI());
+
+    //
+    // ROM patches
+    //
+  }
+
+}
---
+++ src/test/java/de/heiden/jem/models/c64/Testsuite2_15.java
@@ -0,0 +1,13 @@
+package de.heiden.jem.models.c64;
+
+import java.net.URL;
+
+/**
+ * Testsuite 2.15.
+ */
+public class Testsuite2_15 {
+  public static void main(String[] args) {
+    URL start = Testsuite2_15.class.getResource("/testsuite2.15/ start.prg");
+    System.out.println(start);
+  }
+}
