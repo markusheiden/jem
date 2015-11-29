fd90224b024c7157568975de5902630cc79f5dca
Markus Heiden <markus@markusheiden.de>
2015-11-29 02:00:33 +0100

    Rework clock test

--- src/test/java/de/heiden/jem/components/clock/ClockTestBase.java
+++ src/test/java/de/heiden/jem/components/clock/ClockTestBase.java
@@ -1,6 +1,5 @@
 package de.heiden.jem.components.clock;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,32 +8,6 @@ import static org.junit.Assert.assertEquals;
  * Test base for {@link Clock}s.
  */
 public abstract class ClockTestBase {
-  /**
-   * Clock under test.
-   */
-  private Clock clock;
-
-  /**
-   * Counter components.
-   */
-  private CounterComponent[] counters;
-
-  /**
-   * Set up.
-   */
-  @Before
-  public void setUp() {
-    clock = createClock();
-
-    int num = Runtime.getRuntime().availableProcessors() * 3;
-    counters = new CounterComponent[num];
-    for (int i = 0; i < num; i++) {
-      CounterComponent counter = new CounterComponent();
-      clock.addClockedComponent(i, counter);
-      counters[i] = counter;
-    }
-  }
-
   /**
    * Create clock.
    */
@@ -45,8 +18,30 @@ public abstract class ClockTestBase {
    */
   @Test
   public void run() {
+    int runs = 10;
     int cycles = 10_000;
+    for (int i = 0; i < runs; i++) {
+       run(cycles);
+    }
+  }
+
+  /**
+   * Test run clock.
+   */
+  private void run(int cycles) {
+    Clock clock = createClock();
+
+    int num = Runtime.getRuntime().availableProcessors() * 3;
+    CounterComponent[] counters = new CounterComponent[num];
+    for (int i = 0; i < num; i++) {
+      CounterComponent counter = new CounterComponent();
+      clock.addClockedComponent(i, counter);
+      counters[i] = counter;
+    }
+
     clock.run(cycles);
+
+    // Check that al components are executed exactly the specified amount of cycles.
     for (int i = 0; i < counters.length; i++) {
       assertEquals(cycles, counters[i].getCount());
     }
