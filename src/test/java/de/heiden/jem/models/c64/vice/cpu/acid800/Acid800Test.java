71ce7e24954cc70f67f4879d3945a036f954bf44
Markus Heiden <markus@markusheiden.de>
2016-08-11 23:07:22 +0200

    Add another test

--- src/test/java/de/heiden/jem/models/c64/vice/cpu/acid800/Acid800Test.java
+++ src/test/java/de/heiden/jem/models/c64/vice/cpu/acid800/Acid800Test.java
@@ -11,15 +11,28 @@ import static org.junit.Assert.assertEquals;
  * VICE test suite.
  */
 @RunWith(TransformingRunner.class)
-public class Acid800Suite extends AbstractTest {
+public class Acid800Test extends AbstractTest {
   @Test
   public void cpuDecimal() throws Exception {
     loadAndRun("/vice/cpu/acid800/cpu-decimal.prg");
 
-    int result = waitSecondsFor(100);
+    int result = waitSecondsFor(1);
 
     String console = captureScreen();
     System.out.println(console);
+
+    assertEquals(WAIT_PROGRAM_END, result);
+  }
+
+  @Test
+  public void cpuInsn() throws Exception {
+    loadAndRun("/vice/cpu/acid800/cpu-insn.prg");
+
+    int result = waitSecondsFor(10);
+
+    String console = captureScreen();
+    System.out.println(console);
+
     assertEquals(WAIT_PROGRAM_END, result);
   }
 }
