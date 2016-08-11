1f2f5f85689f8af2c4579e9f295eaf6b0b2e12ab
Markus Heiden <markus@markusheiden.de>
2016-08-11 23:29:37 +0200

    Fix trace output

--- src/main/java/de/heiden/jem/models/c64/components/cpu/Trace.java
+++ src/main/java/de/heiden/jem/models/c64/components/cpu/Trace.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import static de.heiden.c64dt.assembler.Opcode.OPCODES;
+import static java.lang.System.arraycopy;
 
 /**
  * Data of a single executed opcode for execution tracing.
@@ -22,20 +23,26 @@ public class Trace {
   public int address;
 
   /**
+   * Size of opcode incl. argument.
+   */
+  public int size = 0;
+
+  /**
    * Byte representation of opcode incl. argument.
    */
-  public final byte[] bytes = new byte[3];
+  public byte[] bytes = new byte[3];
 
   /**
    * Read trace data from bus.
    *
-   * @param pc PC, address to read from
-   * @param bus Bus
+   * @param pc PC, address to read from.
+   * @param bus Bus.
    */
   public final void read(int pc, BusDevice bus) {
     address = pc;
 
     Opcode opcode = OPCODES[bus.read(pc)];
+    size = opcode.getSize();
     for (int i = 0; i < opcode.getSize(); i++) {
       bytes[i] = (byte) bus.read(pc);
       pc = (pc + 1) & 0xFFFF;
@@ -46,14 +53,24 @@ public class Trace {
    * Return opcode representation as CodeBuffer.
    */
   public ICodeBuffer toCodeBuffer() {
-    return new CodeBuffer(address, bytes);
+    byte[] truncated = bytes;
+    if (truncated.length > size) {
+      truncated = new byte[size];
+      arraycopy(bytes, 0, truncated, 0, size);
+    }
+    return new CodeBuffer(address, truncated);
   }
+
+  /**
+   * Disassembler for {@link #toString()}.
+   */
+  private static final Disassembler disassembler = new Disassembler();
 
   @Override
   public String toString() {
     try {
-      StringWriter result = new StringWriter(16);
-      new Disassembler().disassemble(toCodeBuffer(), result);
+      StringWriter result = new StringWriter(40);
+      disassembler.disassemble(toCodeBuffer(), result);
       return result.toString();
     } catch (IOException e) {
       return "???";
