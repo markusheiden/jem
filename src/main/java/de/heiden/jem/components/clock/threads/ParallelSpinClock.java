de35cc91f243372b84e899d55e33638a3b022590
Markus Heiden <markus@markusheiden.de>
2019-12-25 12:39:47 +0100

    Optimize thread switch

--- src/main/java/de/heiden/jem/components/clock/threads/ParallelSpinClock.java
+++ src/main/java/de/heiden/jem/components/clock/threads/ParallelSpinClock.java
@@ -91,9 +91,9 @@ public final class ParallelSpinClock extends AbstractSynchronizedClock {
     @Override
     public final void waitForTick() {
       _state = true;
-      while (_state) {
+      do {
         Thread.yield();
-      }
+      } while (_state);
     }
 
     /**
