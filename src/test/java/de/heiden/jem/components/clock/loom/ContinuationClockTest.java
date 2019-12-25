36f9f40b5a291d4ea5b13dd2e4ff344a20ce55bc
Markus Heiden <markus@markusheiden.de>
2019-12-25 14:20:01 +0100

    Add test

---
+++ src/test/java/de/heiden/jem/components/clock/loom/ContinuationClockTest.java
@@ -0,0 +1,14 @@
+package de.heiden.jem.components.clock.loom;
+
+import de.heiden.jem.components.clock.Clock;
+import de.heiden.jem.components.clock.ClockTestBase;
+
+/**
+ * Test for {@link ContinuationClock}.
+ */
+class ContinuationClockTest extends ClockTestBase {
+  @Override
+  protected Clock createClock() {
+    return new ContinuationClock();
+  }
+}
