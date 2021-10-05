b3b32cda968514a5198c33a28db99949e799b5da
Markus Heiden <markus@markusheiden.de>
2021-10-05 22:33:43 +0200

    Remove unused code

--- src/main/java/de/heiden/jem/components/clock/loom/SequentialFiberExecutorClock.java
+++ src/main/java/de/heiden/jem/components/clock/loom/SequentialFiberExecutorClock.java
@@ -1,9 +1,6 @@
 package de.heiden.jem.components.clock.loom;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
 
 import de.heiden.jem.components.clock.AbstractSimpleClock;
 import de.heiden.jem.components.clock.ClockedComponent;
@@ -12,14 +9,9 @@ import de.heiden.jem.components.clock.ClockedComponent;
  * Clock executing {@link Thread fibers} of project loom on an {@link Executor}.
  */
 public final class SequentialFiberExecutorClock extends AbstractSimpleClock {
-    /**
-     * The one and only thread to execute the fibers.
-     */
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
-
     @Override
     protected void doRun() {
-        var fibers = createFibers();
+        var fibers = createVirtualThreads();
         while (true) {
             startTick();
             for (var thread : fibers) {
@@ -32,7 +24,7 @@ public final class SequentialFiberExecutorClock extends AbstractSimpleClock {
     protected void doRun(int ticks) {
         assert ticks >= 0 : "Precondition: ticks >= 0";
 
-        var fibers = createFibers();
+        var fibers = createVirtualThreads();
         for (final long stop = getTick() + ticks; getTick() < stop; ) {
             startTick();
             for (var thread : fibers) {
@@ -42,56 +34,28 @@ public final class SequentialFiberExecutorClock extends AbstractSimpleClock {
     }
 
     /**
-     * Create fibers.
+     * Create a virtual thread for each component.
      */
-    private Thread[] createFibers() {
+    private Thread[] createVirtualThreads() {
         var components = _componentMap.values().toArray(ClockedComponent[]::new);
 
-        var fibers = new Thread[components.length];
+        var virtualThreads = new Thread[components.length];
         for (int i = 0; i < components.length; i++) {
             var component = components[i];
             component.setTick(Thread::yield);
-            fibers[i] = Thread.ofVirtual()
-                    .scheduler(executor)
-                    .uncaughtExceptionHandler(this::uncaughtException)
-                    .start(() -> {
-                        Thread.yield();
-                        component.run();
-                    });
+            virtualThreads[i] = Thread.ofVirtual().start(() -> {
+                Thread.yield();
+                component.run();
+            });
         }
 
+        // Let the thread run into the first yield().
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             // Ignore.
         }
 
-        return fibers;
-    }
-
-    @Override
-    protected void doClose() {
-        executor.shutdown();
-    }
-
-    /**
-     * Ignore {@link RejectedExecutionException}s which are caused by the executor being shutdown.
-     * Rethrows all other exceptions.
-     */
-    private void uncaughtException(Thread t, Throwable e) {
-        if (e instanceof Error error) {
-            throw error;
-        }
-
-        if (e instanceof RejectedExecutionException) {
-            // Thread pool has been closed. Abort execution.
-            return;
-        }
-
-        if (e instanceof RuntimeException runtime) {
-            throw runtime;
-        }
-
-        throw new RuntimeException(e);
+        return virtualThreads;
     }
 }
