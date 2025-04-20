13f6dde5f5e48ed99bafa6d7d9bd007fca568eda
Markus Heiden <markus@markusheiden.de>
2025-04-20 22:19:19 +0200

    Use more var

--- src/main/java/de/heiden/jem/models/c64/components/patch/LoadFile.java
+++ src/main/java/de/heiden/jem/models/c64/components/patch/LoadFile.java
@@ -8,10 +8,12 @@ import de.heiden.jem.models.c64.components.cpu.Patch;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import static de.heiden.jem.models.c64.components.patch.FileUtil.read;
+
 /**
  * Replaces standard C64 load routine at $F4A5.
  * Intercepts load routine at $F4C4 directly after printing "SEARCHING FOR".
- * Loads a fixed file.
+ * Load a fixed file.
  */
 public class LoadFile extends Patch {
   /**
@@ -32,13 +34,13 @@ public class LoadFile extends Patch {
 
   @Override
   protected int execute(CPU6510State state, BusDevice bus) {
-    WordBus wordBus = new WordBus(bus);
+    var wordBus = new WordBus(bus);
 
     try {
-      ByteArrayInputStream file = new ByteArrayInputStream(content);
+      var file = new ByteArrayInputStream(content);
       int endAddress = bus.read(0xB9) == 0 ?
-        FileUtil.read(file, wordBus.readWord(0xC3), bus) :
-        FileUtil.read(file, bus);
+        read(file, wordBus.readWord(0xC3), bus) :
+        read(file, bus);
 
       wordBus.writeWord(0xAE, endAddress);
 
