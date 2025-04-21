cd85c23bd2c07eda85fccc535572f0ab19693352
Markus Heiden <markus@markusheiden.de>
2025-04-21 16:05:39 +0200

    Reduce code duplication

--- src/main/java/de/heiden/jem/models/c64/components/patch/FileUtil.java
+++ src/main/java/de/heiden/jem/models/c64/components/patch/FileUtil.java
@@ -1,6 +1,5 @@
 package de.heiden.jem.models.c64.components.patch;
 
-import de.heiden.c64dt.bytes.ByteUtil;
 import de.heiden.jem.components.bus.BusDevice;
 
 import java.io.BufferedInputStream;
@@ -8,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 
+import static de.heiden.c64dt.bytes.ByteUtil.toWord;
 import static java.nio.file.Files.newInputStream;
 
 /**
@@ -18,12 +18,13 @@ public class FileUtil {
    * Read the file and write it to memory.
    * The first word in the file is interpreted as the start address.
    *
-   * @param file File
-   * @param bus Bus with memory
-   * @return End address
+   * @param file File.
+   * @param bus Bus with memory.
+   * @return End address.
    */
   public static int read(Path file, BusDevice bus) throws IOException {
     assert file != null : "file != null";
+    assert bus != null : "bus != null";
 
     return read(newInputStream(file), bus);
   }
@@ -32,58 +33,61 @@ public class FileUtil {
    * Read the file and write it to memory.
    * The first word in the file is interpreted as the start address.
    *
-   * @param file File
-   * @param bus Bus with memory
-   * @return End address
+   * @param file File.
+   * @param bus Bus with memory.
+   * @return End address.
    */
   public static int read(InputStream file, BusDevice bus) throws IOException {
     assert file != null : "file != null";
     assert bus != null : "bus != null";
 
     try (var is = new BufferedInputStream(file)) {
-      int addr = ByteUtil.toWord(is.read(), is.read());
-      for (int b; (b = is.read()) >= 0; addr++) {
-        bus.write(b, addr);
-      }
-
-      return addr;
+      int addr = toWord(is.read(), is.read());
+      return readAll(is, addr, bus);
     }
   }
 
   /**
    * Read the file and write it to memory.
-   * Ignores the first word.
+   * Ignores the first word (start address).
    *
-   * @param file File
-   * @param addr Start address
-   * @param bus Bus with memory
-   * @return End address
+   * @param file File.
+   * @param addr Start address.
+   * @param bus Bus with memory.
+   * @return End address.
    */
   public static int read(Path file, int addr, BusDevice bus) throws IOException {
+    assert file != null : "file != null";
+    assert bus != null : "bus != null";
+
     return read(newInputStream(file), addr, bus);
   }
 
   /**
    * Read the file and write it to memory.
-   * Ignores the first word.
+   * Ignores the first word (start address).
    *
-   * @param file File
-   * @param addr Start address
-   * @param bus Bus with memory
-   * @return End address
+   * @param file File.
+   * @param addr Start address.
+   * @param bus Bus with memory.
+   * @return End address.
    */
   public static int read(InputStream file, int addr, BusDevice bus) throws IOException {
     assert file != null : "file != null";
     assert bus != null : "bus != null";
 
     try (var is = new BufferedInputStream(file)) {
+      // Skip address.
       is.read();
       is.read();
-      for (int b; (b = is.read()) >= 0; addr++) {
-        bus.write(b, addr);
-      }
-
-      return addr;
+      return readAll(is, addr, bus);
     }
+  }
+
+  private static int readAll(InputStream is, int addr, BusDevice bus) throws IOException {
+    for (int b; (b = is.read()) >= 0; addr++) {
+      bus.write(b, addr);
+    }
+    return addr;
   }
 }
